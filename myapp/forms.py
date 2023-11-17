ff404991040cbddce60b27d23c4811f61b611af5
Sebastián Clement <sebasclement.sc@gmail.com>
2023-11-17 14:42:30 -0300

    cuarto commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Título de Tarea: ", max_length=200, widget=forms.TextInput)
+    title = forms.CharField(label="Título de Tarea: ", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
     description = forms.CharField(label='Descripción de la tarea:',widget=forms.TextInput(attrs={'class':'input'}))
     project_id = forms.CharField(label="Proyecto", widget=forms.Select)
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label='Nombre del proyecto', max_length=200)
+    name = forms.CharField(label='Nombre del proyecto', max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('hello/', views.hello, name='hello'),
     path('about/', views.about, name='about'),
     path('project/', views.project, name='project'),
